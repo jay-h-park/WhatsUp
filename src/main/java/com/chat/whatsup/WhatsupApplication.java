1c14fc1130ea52d9dda23b0a7a0bca9c57d0a726
JaehongPARK <woghd8754@gmail.com>
2021-05-16 19:16:53 +0900

    Initial commit

---
+++ src/main/java/com/chat/whatsup/WhatsupApplication.java
@@ -0,0 +1,13 @@
+package com.chat.whatsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/whatsup/WhatsupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.whatsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
