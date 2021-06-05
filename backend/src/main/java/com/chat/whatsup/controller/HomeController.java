4f37519325afcd2865390613754f1c8f9d0f370e
JaehongParkYS <woghd8754@gmail.com>
2021-06-05 18:06:49 +0900

    feat: HomeController 추가
    Hello World 화면을 보여주기 위한 HomeController를 추가하였음. 이는
    root directory(`/`)로 접속할 경우 static/index.html 경로의 페이지를
    로드하도록 구성함.

---
+++ backend/src/main/java/com/chat/whatsup/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.chat.whatsup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
